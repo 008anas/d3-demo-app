671206ebdead04d68d3c5eddf4cadd3cd8e1990f
13anas <anas.gharrab@gmail.comb>
2020-01-27 16:19:26 +0100

    Prod endpoint

--- web_app/frontend/src/environments/environment.prod.ts
+++ web_app/frontend/src/environments/environment.prod.ts
@@ -9,6 +9,6 @@ export const environment = {
     dsn: 'https://f35c3be6cd794b069c754d34e0daa7a9@sentry.io/1552997'
   },
   endpoints: {
-    api: 'http://bioroboost.crg.es/api/v1',
+    api: 'http://sqrutiny.crg.es/api/v1',
   }
 };
