fc22b9d501456cd2a341a0a9390bd5ba6e1def5b
PC <pc@iMac-de-PC.local>
2020-03-27 17:12:58 +0100

    Accept

--- web_app/backend/sqrutiny/settings.py
+++ web_app/backend/sqrutiny/settings.py
@@ -78,9 +78,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer'
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-    ),
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
--- web_app/frontend/src/app/core/app.interceptor.ts
+++ web_app/frontend/src/app/core/app.interceptor.ts
@@ -2,14 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 
+import { CookieService } from 'ngx-cookie-service';
+
 @Injectable()
 export class AppHttpInterceptor implements HttpInterceptor {
+
+  constructor(private cookieSrvc: CookieService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     request = request.clone({
       withCredentials: true
     });
+
+    if (!request.headers.has('Accept')) {
+      request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
+    }
+
+    if (this.cookieSrvc.get('csrftoken')) {
+      request = request.clone({ headers: request.headers.set('X-CSRFToken', this.cookieSrvc.get('csrftoken')) });
+    }
 
     return next.handle(request);
   }
