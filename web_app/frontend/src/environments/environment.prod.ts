aa6b95fff03db50e092544af0705cd2d92a14cf3
PC <pc@iMac-de-PC.local>
2020-03-28 18:34:49 +0100

    Change endpoint

--- web_app/frontend/src/environments/environment.prod.ts
+++ web_app/frontend/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     dsn: 'https://f35c3be6cd794b069c754d34e0daa7a9@sentry.io/1552997'
   },
   endpoints: {
-    api: 'http://sqrutiny.crg.es/api/v1',
+    api: 'http://sqrutiny.crg.es:8001/api/v1',
   }
 };
