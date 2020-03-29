926d1d0df46a83500fee704137d124e1047a85f2
PC <pc@iMac-de-PC.local>
2020-03-29 23:41:14 +0200

    Middleware

--- web_app/backend/sqrutiny/settings.py
+++ web_app/backend/sqrutiny/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'x_forwarded_for.middleware.XForwardedForMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
