08ee1cb3ac11e899e3486219f2626c9160bd4c65
PC <pc@iMac-de-PC.local>
2020-03-31 18:16:34 +0200

    urls

--- web_app/backend/sqrutiny/urls.py
+++ web_app/backend/sqrutiny/urls.py
@@ -9,11 +9,9 @@ from . import settings
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('admin/django-rq', include('django_rq.urls'))
+    path('admin/django-rq', include('django_rq.urls')),
+    path('api/v1/', include('app.urls'))
 ]
-
-if not settings.DEBUG:
-    urlpatterns += path('api/v1/', include('app.urls'))
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
