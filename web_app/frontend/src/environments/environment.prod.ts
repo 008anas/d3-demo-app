82b55d14201cdde93b2b40d2abdc2c3b79b7d3e4
anas.gharrab@saedpvcom <abdellah008>
2021-04-18 18:52:00 +0200

    Endpoint changed.

--- web_app/frontend/src/environments/environment.prod.ts
+++ web_app/frontend/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     dsn: 'https://5a5abc10ac0a4231906f42c6a3e990de@sentry.io/5186008'
   },
   endpoints: {
-    api: 'http://sqrutiny.crg.es/api/v1',
+    api: 'http://sqrutiny.crg.eu/api/v1',
   }
 };
